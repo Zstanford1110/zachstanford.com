47ca9d623fc8b0df8441f55d8c9ba06de2b9e325
Zstanford1110 <zachary.s1110@gmail.com>
2025-03-05 22:27:50 -0500

    Removed Tailwind from Component Library

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@repo/ui';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900 px-4">
       <Button className={'px-4 py-2 bg-blue-500 rounded text-white'} appName='Next.js'>Click Me</Button>
     </main>
   );
